b58668cb25e7a67c672ce340e48939b741889b96
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2023-07-31 10:05:33 -0300

    create loading component and theme

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,5 @@
 import * as S from './styles'
 
 export function Groups() {
-  return (
-    <S.Container>
-      <S.Text>Groups</S.Text>
-    </S.Container>
-  )
+  return <S.Container></S.Container>
 }
