6a4543a736937f549af57ee48eef00bdd08a12fd
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2023-08-24 12:28:59 -0300

    add groups in storage

--- src/screens/NewGroup/Index.tsx
+++ src/screens/NewGroup/Index.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
+import { Alert } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+import { groupCreate } from '@storage/group/groupCreate'
+import { AppError } from '@utils/AppError'
 import * as S from './styles'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
@@ -10,8 +13,21 @@ export function NewGroup() {
   const [group, setGroup] = useState('')
   const navigation = useNavigation()
 
-  function handleNew() {
-    navigation.navigate('players', { group })
+  async function handleNew() {
+    try {
+      if (group.trim().length === 0) {
+        return Alert.alert('Novo Grupo', 'Informe o nome da turma.')
+      }
+      await groupCreate(group.trim())
+      navigation.navigate('players', { group })
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message)
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.')
+        console.log(error)
+      }
+    }
   }
 
   return (
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
-import { FlatList } from 'react-native'
+import { Alert, FlatList } from 'react-native'
+import { useRoute } from '@react-navigation/native'
+import { AppError } from '@utils/AppError'
 import * as S from './styles'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
@@ -9,7 +11,8 @@ import { Filter } from '@components/Filter'
 import { PlayerCard } from '@components/PlayerCard'
 import { ListEmpty } from '@components/ListEmpty'
 import { Button } from '@components/Button'
-import { useRoute } from '@react-navigation/native'
+import { playerAddByGroup } from '@storage/player/playerAddByGroup'
+import { playersGetByGroup } from '@storage/player/playersGetByGroup'
 
 type RouteParams = {
   group: string
@@ -18,17 +21,49 @@ type RouteParams = {
 export function Players() {
   const [team, setTeam] = useState('time React')
   const [players, setPlayers] = useState([])
+  const [newPlayerName, setNewPlayerName] = useState('')
 
   const route = useRoute()
   const { group } = route.params as RouteParams
+
+  async function handleAddPlayer() {
+    if (newPlayerName.trimEnd().length === 0) {
+      return Alert.alert(
+        'Nova pessoa',
+        'Informe o nome da pessoa para adicionar',
+      )
+    }
+
+    const newPlayer = {
+      name: newPlayerName.trim(),
+      team,
+    }
+
+    try {
+      await playerAddByGroup(newPlayer, group)
+      const players = await playersGetByGroup(group)
+      console.log(players)
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert('Nova pessoa', error.message)
+      } else {
+        console.log(error)
+        Alert.alert('Nova pessoa', 'Não foi possível adiconar uma nova pessoa.')
+      }
+    }
+  }
 
   return (
     <S.Container>
       <Header showBackButton />
       <Highlight title={group} subtitle="Adicione a galera e separe os times" />
       <S.Form>
-        <Input placeholder="Nome da pessoa" autoCorrect={false} />
-        <ButtonIcon icon="add" />
+        <Input
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+          onChangeText={setNewPlayerName}
+        />
+        <ButtonIcon icon="add" onPress={handleAddPlayer} />
       </S.Form>
 
       <S.HeaderList>
